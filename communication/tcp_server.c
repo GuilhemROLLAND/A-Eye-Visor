141d88bb00112595973c4393352e865342c64bee
Thomas DB <darkchameau@gmail.com>
2022-04-25 16:16:30 +0200

    tcp client working and tcp server for the zybo

--- communication/tcp_server.c
+++ communication/tcp_server.c
@@ -1,244 +1,91 @@
-#include "tcp.h"
-#include "ip4_addr.h"
-#include "ip_addr.h"
-#include "tcpbase.h"
-
-#include "tcp_server.h"
-
-err_t err;
-static struct tcp_pcb *tcp_pcb;
-struct tcp_pcb *pcb;
-const ip_addr_t *localIP = IP4_ADDR_ANY;
-u16_t port = 1028;
-
-
-
-
-// init
-void initTCP(void)
+#include<stdio.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+short SocketCreate(void)
 {
-    lwip_init();
-    sys_check_timeouts();
-    tcp_tmr();
-
-    tcp_pcb = tcp_new_ip_type(IPADDR_TYPE_V4);
-
-    if (tcp_pcb != NULL)
-    {
-        err_t err;
-
-        err = tcp_bind(tcp_pcb, IP4_ADDR_ANY, 7);
-        if (err == ERR_OK)
-        {
-            tcp_pcb = tcp_listen(tcp_pcb);
-            tcp_accept(tcp_pcb, tcp_pcb);
-        }
-    }
+    short hSocket;
+    printf("Create the socket\n");
+    hSocket = socket(AF_INET, SOCK_STREAM, 0);
+    return hSocket;
 }
-
-
-static err_t sentTCP(void *arg, struct tcp_pcb *tpcb, u16_t len)
+int BindCreatedSocket(int hSocket)
 {
-    struct tcp_state *es;
-
-    LWIP_UNUSED_ARG(len);
-
-    es = (struct tcp_state *)arg;
-    es->retries = 0;
-
-    if (es->p != NULL)
-    {
-        /* still got pbufs to send */
-        tcp_sent(tpcb, sentTCP);
-        sendTCP(tpcb, es);
-    }
-    else
-    {
-        /* no more pbufs to send */
-        if (es->state == ES_CLOSING)
-        {
-            closeTCP(tpcb, es);
-        }
-    }
-    return ERR_OK;
+    int iRetval=-1;
+    int ClientPort = 90190;
+    struct sockaddr_in  remote= {0};
+    /* Internet address family */
+    remote.sin_family = AF_INET;
+    /* Any incoming interface */
+    remote.sin_addr.s_addr = htonl(INADDR_ANY);
+    remote.sin_port = htons(ClientPort); /* Local port */
+    iRetval = bind(hSocket,(struct sockaddr *)&remote,sizeof(remote));
+    return iRetval;
 }
-
-static void sendTCP(struct tcp_pcb *tpcb, struct tcp_state *es)
+int main(int argc, char *argv[])
 {
-    struct pbuf *ptr;
-    err_t wr_err = ERR_OK;
-
-    while ((wr_err == ERR_OK) &&
-           (es->p != NULL) &&
-           (es->p->len <= tcp_sndbuf(tpcb)))
+    int socket_desc, sock, clientLen, read_size;
+    struct sockaddr_in server, client;
+    char client_message[200]= {0};
+    char message[100] = {0};
+    const char *pMessage = "hello aticleworld.com";
+    //Create socket
+    socket_desc = SocketCreate();
+    if (socket_desc == -1)
     {
-        ptr = es->p;
-
-        /* enqueue data for transmission */
-        wr_err = tcp_write(tpcb, ptr->payload, ptr->len, 1);
-        if (wr_err == ERR_OK)
+        printf("Could not create socket");
+        return 1;
+    }
+    printf("Socket created\n");
+    //Bind
+    if( BindCreatedSocket(socket_desc) < 0)
+    {
+        //print the error message
+        perror("bind failed.");
+        return 1;
+    }
+    printf("bind done\n");
+    //Listen
+    listen(socket_desc, 3);
+    //Accept and incoming connection
+    while(1)
+    {
+        printf("Waiting for incoming connections...\n");
+        clientLen = sizeof(struct sockaddr_in);
+        //accept connection from an incoming client
+        sock = accept(socket_desc,(struct sockaddr *)&client,(socklen_t*)&clientLen);
+        if (sock < 0)
         {
-            u16_t plen;
-
-            plen = ptr->len;
-            /* continue with next pbuf in chain (if any) */
-            es->p = ptr->next;
-            if (es->p != NULL)
-            {
-                /* new reference! */
-                pbuf_ref(es->p);
-            }
-            /* chop first pbuf from chain */
-            pbuf_free(ptr);
-            /* we can read more data now */
-            tcp_recved(tpcb, plen);
+            perror("accept failed");
+            return 1;
         }
-        else if (wr_err == ERR_MEM)
+        printf("Connection accepted\n");
+        memset(client_message, '\0', sizeof client_message);
+        memset(message, '\0', sizeof message);
+        //Receive a reply from the client
+        if( recv(sock, client_message, 200, 0) < 0)
         {
-            /* we are low on memory, try later / harder, defer to poll */
-            es->p = ptr;
+            printf("recv failed");
+            break;
+        }
+        printf("Client reply : %s\n",client_message);
+        if(strcmp(pMessage,client_message)==0)
+        {
+            strcpy(message,"Hi there !");
         }
         else
         {
-            /* other problem ?? */
+            strcpy(message,"Invalid Message !");
         }
+        // Send some data
+        if( send(sock, message, strlen(message), 0) < 0)
+        {
+            printf("Send failed");
+            return 1;
+        }
+        close(sock);
+        sleep(1);
     }
+    return 0;
 }
-
-static void closeTCP(struct tcp_pcb *tpcb, struct tcp_state *es)
-{
-  tcp_arg(tpcb, NULL);
-  tcp_sent(tpcb, NULL);
-  tcp_recv(tpcb, NULL);
-  tcp_err(tpcb, NULL);
-  tcp_poll(tpcb, NULL, 0);
-
-  freeTCP(es);
-
-  tcp_close(tpcb);
-}
-
-static void errorTCP(void *arg, err_t err)
-{
-  struct tcp_state *es;
-
-  LWIP_UNUSED_ARG(err);
-
-  es = (struct tcp_state *)arg;
-
-  freeTCP(es);
-}
-
-static err_t recvTCP(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err)
-{
-  struct tcp_state *es;
-  err_t ret_err;
-
-  LWIP_ASSERT("arg != NULL",arg != NULL);
-  es = (struct tcp_state *)arg;
-  if (p == NULL) {
-    /* remote host closed connection */
-    es->state = ES_CLOSING;
-    if(es->p == NULL) {
-      /* we're done sending, close it */
-     closeTCP(tpcb, es);
-    } else {
-      /* we're not done yet */
-      sendTCP(tpcb, es);
-    }
-    ret_err = ERR_OK;
-  } else if(err != ERR_OK) {
-    /* cleanup, for unknown reason */
-    LWIP_ASSERT("no pbuf expected here", p == NULL);
-    ret_err = err;
-  }
-  else if(es->state == ES_ACCEPTED) {
-    /* first data chunk in p->payload */
-    es->state = ES_RECEIVED;
-    /* store reference to incoming pbuf (chain) */
-    es->p = p;
-    sendTCP(tpcb, es);
-    ret_err = ERR_OK;
-  } else if (es->state == ES_RECEIVED) {
-    /* read some more data */
-    if(es->p == NULL) {
-      es->p = p;
-      sendTCP(tpcb, es);
-    } else {
-      struct pbuf *ptr;
-
-      /* chain pbufs to the end of what we recv'ed previously  */
-      ptr = es->p;
-      pbuf_cat(ptr,p);
-    }
-    ret_err = ERR_OK;
-  } else {
-    /* unknown es->state, trash data  */
-    tcp_recved(tpcb, p->tot_len);
-    pbuf_free(p);
-    ret_err = ERR_OK;
-  }
-  return ret_err;
-}
-
-static err_t pollTCP(void *arg, struct tcp_pcb *tpcb)
-{
-  err_t ret_err;
-  struct tcp_state *es;
-
-  es = (struct tcp_state *)arg;
-  if (es != NULL) {
-    if (es->p != NULL) {
-      /* there is a remaining pbuf (chain)  */
-      sendTCP(tpcb, es);
-    } else {
-      /* no remaining pbuf (chain)  */
-      if(es->state == ES_CLOSING) {
-        closeTCP(tpcb, es);
-      }
-    }
-    ret_err = ERR_OK;
-  } else {
-    /* nothing to be done */
-    tcp_abort(tpcb);
-    ret_err = ERR_ABRT;
-  }
-  return ret_err;
-}
-
-
-static err_t acceptTCP(void *arg, struct tcp_pcb *newpcb, err_t err)
-{
-  err_t ret_err;
-  struct tcp_state *es;
-
-  LWIP_UNUSED_ARG(arg);
-  if ((err != ERR_OK) || (newpcb == NULL)) {
-    return ERR_VAL;
-  }
-
-  /* Unless this pcb should have NORMAL priority, set its priority now.
-     When running out of pcbs, low priority pcbs can be aborted to create
-     new pcbs of higher priority. */
-  tcp_setprio(newpcb, TCP_PRIO_MIN);
-
-  es = (struct tcp_state *)mem_malloc(sizeof(struct tcp_state));
-  if (es != NULL) {
-    es->state = ES_ACCEPTED;
-    es->pcb = newpcb;
-    es->retries = 0;
-    es->p = NULL;
-    /* pass newly allocated es to our callbacks */
-    tcp_arg(newpcb, es);
-    tcp_recv(newpcb, recvTCP);
-    tcp_err(newpcb, errorTCP);
-    tcp_poll(newpcb, pollTCP, 0);
-    tcp_sent(newpcb, sentTCP);
-    ret_err = ERR_OK;
-  } else {
-    ret_err = ERR_MEM;
-  }
-  return ret_err;
-}
-
-
